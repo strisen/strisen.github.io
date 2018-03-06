1a485556c679e9007e4712ee870212fa27caf61b
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-03-06 16:18:11 +0800

    Updated pictures and scrolling to divs

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,30 @@ $(document).ready(function () {
 
     var screenTop = $(document).scrollTop()
 
+    console.log(screenTop)
+
     if (screenTop === 0) {
       $(".navbar").removeClass("scrolled")
     }
+  })
+
+  $("#ahead").click(function () {
+    $("html, body").animate({
+      scrollTop : $("#header").offset().top }, 500)
+  })
+
+  $("#aabout").click(function () {
+    $("html, body").animate({
+      scrollTop : $("#header").offset().top + 2000 }, 500)
+  })
+
+  $("#aprojects").click(function () {
+    $("html, body").animate({
+      scrollTop : $("#header").offset().top + 547 }, 500)
+  })
+
+  $("#acontact").click(function () {
+    $("html, body").animate({
+      scrollTop : $("#header").offset().top + 2000 }, 500)
   })
 })
